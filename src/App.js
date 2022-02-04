ef512da9a71f6b4c59eebc7c5e9e243d7e3995b6
Ryan Friesen <viniloruido@gmail.com>
2022-02-03 17:58:55 -0800

    Moved package.json dependencies to from the parent folder to the child folder

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import Demo from './Demo.js';
 
 function App() {
   return (
-   <div className="App">
-     <Demo />
-   </div>
+    <div className="App">
+      <p>Is this working?</p>
+      <Demo />
+    </div>
   );
 }
 
